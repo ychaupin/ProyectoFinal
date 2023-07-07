b4fc9a049f85fd43e6156cfabe22143d3fc5ffbc
Your Name <you@example.com>
2023-07-06 19:45:47 -0500

    Tercer Avance

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Responder consultas", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Responder consultas", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Accediendo a foro", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/proyectofinal/MainActivity2.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity2.java
@@ -1,4 +1,35 @@
 package com.example.proyectofinal;
 
-public class MainActivity2 {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private EditText editTextRespuesta;
+    private Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        editTextRespuesta = findViewById(R.id.editTextRespuesta);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String consulta = editTextRespuesta.getText().toString();
+
+                Toast.makeText(MainActivity2.this, "Consulta guardada: " + consulta, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
