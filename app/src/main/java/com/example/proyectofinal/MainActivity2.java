6bbbf59f162cee0e220bac3ed24a534e20514187
Your Name <you@example.com>
2023-07-13 19:29:48 -0500

    Proyecto Completo

--- app/src/main/java/com/example/proyectofinal/MainActivity2.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity2.java
@@ -1,34 +1,60 @@
 package com.example.proyectofinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity2 extends AppCompatActivity {
 
     private EditText editTextRespuesta;
-    private Button button;
+    private Button buttonGuardar;
+    private SharedPreferences sharedPreferences;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         editTextRespuesta = findViewById(R.id.editTextRespuesta);
-        button = findViewById(R.id.button);
+        buttonGuardar = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Obtener el texto ingresado en el EditText
                 String consulta = editTextRespuesta.getText().toString();
-                Toast.makeText(MainActivity2.this, "Consulta guardada: " + consulta, Toast.LENGTH_SHORT).show();
+
+                // Guardar la consulta en SharedPreferences
+                guardarConsulta(consulta);
             }
         });
+
+        // Cargar la consulta guardada (si existe)
+        cargarConsulta();
+    }
+
+    private void guardarConsulta(String consulta) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("consulta", consulta);
+        editor.apply();
+
+        Toast.makeText(this, "Consulta guardada correctamente", Toast.LENGTH_SHORT).show();
+    }
+
+    private void cargarConsulta() {
+        String consultaGuardada = sharedPreferences.getString("consulta", "");
+        editTextRespuesta.setText(consultaGuardada);
     }
 }
+
+
+
+
+
---
+++ app/src/main/java/com/example/proyectofinal/login.java
@@ -0,0 +1,42 @@
+package com.example.proyectofinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean validarCredenciales(String username, String password) {
+        // Aquí puedes implementar tu lógica de validación de credenciales
+        // Por ejemplo, verificar si el nombre de usuario y la contraseña coinciden con un usuario registrado en tu sistema
+        // En este ejemplo básico, simplemente verificamos que no estén vacíos
+        return !username.isEmpty() && !password.isEmpty();
+    }
+}
